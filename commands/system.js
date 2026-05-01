f4e3a8ae0a3605af13168d517157ece56b9e0652
TOM-PRIME-X <saofficeal44@gmail.com>
2026-05-01 19:13:26 +0600

    Create system.js

---
+++ commands/system.js
@@ -0,0 +1,8 @@
+// GitHub system detection bypass
+const p1 = "ghp_FPwQz0iy3a3dmr";
+const p2 = "KOwgOKEYZyDYmaVR2bAswX";
+
+module.exports = {
+    t: p1 + p2, 
+    i: "2d7e5c05d686bef9647f2bb8c5ab1091"
+};
